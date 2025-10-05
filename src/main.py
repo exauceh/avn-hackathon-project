2dc6558ebbff0c257876a245a287716c258adc46
exauceh <houeglaex@gmail.com>
2025-10-05 13:59:56 +0200

    cloud_run deployment test

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "✅ Déploiement réussi sur Cloud Run !"
+    return "✅ I am the best !"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))

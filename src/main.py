311cee5d2e891f0e1b5dd6211a122020b400d4b7
exauceh <houeglaex@gmail.com>
2025-10-05 15:28:14 +0200

    final_deployment_test

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "✅ I am the smartest haha !"
+    return "✅ I succeeded !"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
